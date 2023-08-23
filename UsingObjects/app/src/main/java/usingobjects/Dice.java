c54447ecc2345657ccc284906204853310aa0c5a
Turtgun <icisraelcarrillo@gmail.com>
2023-08-23 13:59:17 -0500

    ok

---
+++ UsingObjects/app/src/main/java/usingobjects/Dice.java
@@ -0,0 +1,7 @@
+package usingobjects;
+
+public class Dice {
+    int roll() {
+        return (int)(Math.random()*6) + 1;
+    }
+}
---
+++ UsingObjects/app/src/main/java/usingobjects/UsingObjects.java
@@ -0,0 +1,42 @@
+package usingobjects;
+
+import java.util.Scanner;
+
+/**
+ * Write a description of class UsingObjects here.
+ *
+ * @author Turtgun
+ * @version 0.0.1
+ */
+public class UsingObjects
+{
+    /*
+     * Method will prompt the user on how many times to roll a dice object
+     * and display the value of each roll
+     */
+    public static void rollDice()
+    {
+        Scanner kybd = new Scanner(System.in);
+        System.out.print("Enter the number of rolls: ");
+        int rolls = kybd.nextInt();
+        Dice michael = new Dice();
+        int totalRollResult = 0;
+        int result;
+        for (int i = 0; i < rolls; i++) {
+            result = michael.roll();
+            System.out.println("Result: " + result);
+            totalRollResult += result;
+        }
+        System.out.println("Average result of rolls: " + (double)totalRollResult/rolls + " from " + rolls + " rolls.");
+    }
+    
+    /**
+     * Method will prompt the user for the coordinates of two point 
+     * objects, create them and then calculate the distance betweeen them
+     */
+    public static void calcDistance()
+    {
+        
+    }
+    
+}
